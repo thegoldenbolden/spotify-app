8520e4b6d356bdbcea3faa2fa77287908aa8233f
Jacob Bolden <jlbolden.pro@gmail.com>
2022-11-04 01:42:17 -0500

    initial commit for next v13

--- src/components/error.tsx
+++
@@ -1,2 +0,0 @@
-const Error = () => <div className="grid w-full h-full place-content-center">We failed to get your request. D:</div>;
-export default Error;
--- src/components/loading.tsx
+++
@@ -1,2 +0,0 @@
-const Loading = () => <div className="grid w-full h-full place-content-center">Loading...</div>;
-export default Loading;
